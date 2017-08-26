c46cc14211e6f9fe8dd2a8bb774f3c7d09c742d6
Pablo Angiorama <pabloangiorama@gmail.com>
2017-08-26 14:49:30 -0300

    add /hotel page

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+// import NavLink from '../NavLink'
 
 class Hotel extends Component {
   render() {
     return (
-		<div className="center">
-			<h2>Hotel</h2>
-		</div>
+    	<div className='center'>
+	        <img className="img-responsive" alt=''
+	            src={require('../assets/hotel/header.png')} />
+	    	<div className='padding'>
+	    		<p className=''>250 llaves de la cadena de hoteles Bern 
+	    		donde podrán hospedarse sus familiares, ejecutivos o turistas
+	    		que visiten Panamá</p>
+	        </div>
+	    	<div className='padding'>
+				<button type="button" className='btn btn-lg more-info-btn'>Más información</button>
+	        </div>
+        </div>
     );
   }
 }
