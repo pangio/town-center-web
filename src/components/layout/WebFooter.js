f5f31c8cfd955a42ddf2f1687577c8c8b9b11964
Pablo Angiorama <pabloangiorama@gmail.com>
2017-08-27 19:36:00 -0300

    navbar footer fixes

--- src/components/layout/WebFooter.js
+++ src/components/layout/WebFooter.js
@@ -5,29 +5,29 @@ import '../../main.css';
 class WebFooter extends React.Component {
   render() {
     return (
-      <div className="center footer-color padding">
-        <div className='row center'>
+      <div className='center footer-color'>
+        <div className='row center padding-v'>
             <span className='footer-imgs'>
-                <NavLink to="/" >
-                    <img className="img-responsive donde-img" alt=''
+                <NavLink to='/' >
+                    <img className='img-responsive donde-img' alt=''
                         src={require('../../assets/home/img_donde.png')} />
                 </NavLink>
-                <NavLink to="/" >
-                    <img className="img-responsive donde-img" alt=''
+                <NavLink to='/' >
+                    <img className='img-responsive donde-img' alt=''
                         src={require('../../assets/home/btn_estasconsuerte.png')} />
                 </NavLink>
             </span>
         </div>
-        <div className='row center'>
-            <NavLink to="/" >
+        <div className='row center padding-v'>
+            <NavLink to='/' >
                 <img className='footer-icon' alt=''
                     src='https://image.flaticon.com/icons/svg/124/124010.svg' />
             </NavLink>
-            <NavLink to="/" >
+            <NavLink to='/' >
                 <img className='footer-icon' alt=''
                     src='https://image.flaticon.com/icons/svg/61/61164.svg' />
             </NavLink>
-            <a className='footer-url' href="/"><strong>towncenter.com.pa</strong></a>
+            <a className='footer-url' href='/'><strong>towncenter.com.pa</strong></a>
         </div>
       </div>
     )
