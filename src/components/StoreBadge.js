8c47225b86fc442e4ebb6dea4d4f4c29f56d5b99
Pablo Angiorama <pabloangiorama@gmail.com>
2017-09-03 19:10:12 -0300

    store badge styling

--- src/components/StoreBadge.js
+++ src/components/StoreBadge.js
@@ -3,21 +3,26 @@ import React from 'react';
 class StoreBadge extends React.Component {
   render() {
 	const s3url = 'https://s3.amazonaws.com/towncenterweb/tiendas/'
+	const assetsurl = 'https://s3.amazonaws.com/towncenterweb/assets/'
+    const style = {
+	  padding: 1
+	}
     return (
 		<div className='tienda-box badge'>
-			<div className='row'>
-				<div>
-			        <img className='store-sm-img img-responsive col-xs-4' alt=''
-						src={s3url + this.props.store.image_url} />
-		        </div>
-				<div className='col-xs-7 store-sm-txt'>
-		        	<p><strong>{this.props.store.name}</strong></p>
-		        	<p>{this.props.store.hours}</p>
-		        	<p>{this.props.store.type}</p>
-		        	<p>{this.props.store.level} - {this.props.store.local}</p>
-		        	<br />
-		    	</div>
+			<img className='img-responsive col-xs-4' alt=''
+				style={style}
+				src={s3url + this.props.store.image_url} />
+			<div className='col-xs-7 store-sm-txt'>
+				<p><strong>{this.props.store.name}</strong></p>
+				<p>{this.props.store.hours}</p>
+				<br />
 	    	</div>
+
+			<div className='col-xs-12 center padding-v store-badge-location'>
+		        <img alt='' src={assetsurl + 'ic_geo.png'} />
+		        <span className='lightgray'>{this.props.store.level} - {this.props.store.local}</span>
+	    	</div>
+
     	</div>
     )
   }
