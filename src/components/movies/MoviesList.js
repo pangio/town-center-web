9d7d8d1dec34d482cb0335a65cd02b9167627535
Pablo Angiorama <pabloangiorama@gmail.com>
2017-09-08 00:17:06 -0300

    cinema: order  movies

--- src/components/movies/MoviesList.js
+++ src/components/movies/MoviesList.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import SliderMovies from './SliderMovies'
+import _ from 'underscore'
 
 class MoviesList extends React.Component {
   render() {
+	let moviesOrderedByDate = _.sortBy(this.props.moviesList, 'date').reverse()
+
     return (
       <div className='row center'>
-        <SliderMovies moviesList={this.props.moviesList} />
+        <SliderMovies moviesList={moviesOrderedByDate} />
       </div>
     )
   }
